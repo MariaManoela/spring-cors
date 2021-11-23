660a81ebf9c97bacf3756b49271181a9a445eece
Maria Manoela <manoelamvc@hotmail.com>
2021-11-23 14:19:23 -0300

    :tada: First commit

---
+++ src/test/java/springExercicio03/demo/RestServiceCorsApplicationTests.java
@@ -0,0 +1,13 @@
+package springExercicio03.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestServiceCorsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
