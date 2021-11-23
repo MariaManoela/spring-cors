f266b32869c696d2f2bc45c3509e446c2873eb24
Maria Manoela <manoelamvc@hotmail.com>
2021-11-23 14:59:17 -0300

    Adding global cors

--- src/main/java/springExercicio03/demo/restservicecors/GreetingController.java
+++ src/main/java/springExercicio03/demo/restservicecors/GreetingController.java
@@ -12,10 +12,18 @@ public class GreetingController {
     private static final String template = "Hello, %s!";
 
     private final AtomicLong counter = new AtomicLong();
+
     @CrossOrigin(origins = "http://localhost:8080")
     @GetMapping("/greeting")
     public Greeting greeting(@RequestParam(required = false, defaultValue = "World") String name) {
         System.out.println("==== get greeting ====");
         return new Greeting(counter.incrementAndGet(), String.format(template, name));
     }
+
+    @GetMapping("/greeting-javaconfig")
+    public Greeting greetingWithJavaconfig(@RequestParam(required = false, defaultValue = "World") String name) {
+        System.out.println("==== in greeting ====");
+        return new Greeting(counter.incrementAndGet(), String.format(template, name));
+    }
 }
+
